e9d9bdd2de01494cc6bad90c62e87a8dbac47121
harshpathakzz <harshpathak2017@gmail.com>
2023-06-05 22:59:14 +0530

    UserContext is created

--- src/context/UserAuthContext.jsx
+++ src/context/UserAuthContext.jsx
@@ -1,9 +1,95 @@
-import { createContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../firebase/firebaseConfig";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
 
 const userAuthContext = createContext(null);
 
 export const UserAuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+
+    // Cleanup the subscription on unmount
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const handleSignup = async (email, password) => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      // Signed in
+      const user = userCredential.user;
+      console.log("User created:", user);
+      // ...
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log("Error:", errorCode, errorMessage);
+      // ..
+    }
+  };
+
+  const handleLogin = async (email, password) => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      // Signed in
+      const user = userCredential.user;
+      console.log("User logged in:", user);
+      // ...
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log("Error:", errorCode, errorMessage);
+      // ..
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      console.log("User logged out");
+      // ...
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log("Error:", errorCode, errorMessage);
+      // ..
+    }
+  };
+
+  // Add any additional authentication-related functions you need
+
   return (
-    <userAuthContext.Provider value={{}}>{children}</userAuthContext.Provider>
+    <userAuthContext.Provider
+      value={{
+        user,
+        handleSignup,
+        handleLogin,
+        handleLogout,
+      }}
+    >
+      {children}
+    </userAuthContext.Provider>
   );
+};
+
+export const useUserAuth = () => {
+  return useContext(userAuthContext);
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./context/authContext.jsx";
+import { UserAuthProvider } from "./context/UserAuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
+      <UserAuthProvider>
         <App />
-      </AuthProvider>
+      </UserAuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
