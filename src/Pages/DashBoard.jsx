3d40ce7020e72e4752542c53501b517918566dde
harshpathakzz <harshpathak2017@gmail.com>
2023-06-10 23:53:52 +0530

    added logout button

--- src/Pages/DashBoard.jsx
+++ src/Pages/DashBoard.jsx
@@ -1,7 +1,23 @@
 import React from "react";
-
+import { useUserAuth } from "../context/UserAuthContext";
+import { useNavigate } from "react-router-dom";
 const DashBoard = () => {
-  return <div>DashBoard</div>;
+  const { handleLogout } = useUserAuth();
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate("/");
+  };
+  const handelSubmit = () => {
+    handleLogout();
+    handleNavigate();
+  };
+
+  return (
+    <>
+      <h1>DashBoard</h1>
+      <button onClick={handelSubmit}>Logout</button>
+    </>
+  );
 };
 
 export default DashBoard;
