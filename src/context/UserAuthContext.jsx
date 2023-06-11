229403df4d411b8e13dbc385bcdc6f96c852d65a
harshpathakzz <harshpathak2017@gmail.com>
2023-06-11 23:42:17 +0530

    updated UserAuthContext

--- src/context/UserAuthContext.jsx
+++ src/context/UserAuthContext.jsx
@@ -1,13 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { auth } from "../firebase/firebaseConfig";
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signInWithPopup,
-  GoogleAuthProvider,
-  signOut,
-  onAuthStateChanged,
-} from "firebase/auth";
+import { listenToAuthChanges } from "../functions/authFunctions";
 
 const userAuthContext = createContext(null);
 
@@ -16,75 +8,24 @@ export const UserAuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      setUser(user);
-      setIsLoggedIn(!!user);
-      console.log("User state changed:", user);
-      // console.log("isLoggedIn state changed:", isLoggedIn);
-    });
+    const unsubscribe = listenToAuthChanges(setUser, setIsLoggedIn);
 
     // Cleanup the subscription on unmount
     return () => unsubscribe();
   }, []);
 
-  const handleSignup = async (email, password) => {
-    const userCredential = await createUserWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
-    // Signed in
-    const user = userCredential.user;
-    console.log("User created:", user);
-    // ...
-  };
-
-  const handleLogin = async (email, password) => {
-    const userCredential = await signInWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
-    // Signed in
-    const user = userCredential.user;
-    console.log("User logged in:", user);
-    // ...
-  };
-
-  const handleGoogleLogin = async () => {
-    const provider = new GoogleAuthProvider();
-    const userCredential = await signInWithPopup(auth, provider);
-    // Signed in with Google
-    const user = userCredential.user;
-    console.log("User logged in with Google:", user);
-    // ...
-  };
-
-  const handleLogout = async () => {
-    await signOut(auth);
-    localStorage.removeItem("isLoggedIn");
-    console.log("User logged out");
-
-    // ...
-  };
-
-  // Save isLoggedIn state to local storage
   useEffect(() => {
     localStorage.setItem("isLoggedIn", JSON.stringify(isLoggedIn));
     console.log(isLoggedIn);
   }, [isLoggedIn]);
-
-  // Add any additional authentication-related functions you need
 
   return (
     <userAuthContext.Provider
       value={{
         user,
         isLoggedIn,
-        handleSignup,
-        handleLogin,
-        handleGoogleLogin,
-        handleLogout,
+        setUser,
+        setIsLoggedIn,
       }}
     >
       {children}
