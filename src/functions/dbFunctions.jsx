678359fdfb9fcb4e32b8644679df13b0cb8d0bf9
harshpathakzz <harshpathak2017@gmail.com>
2023-06-13 10:00:15 +0530

    Fixed TypeError in addUserToCollection function

--- src/functions/dbFunctions.jsx
+++ src/functions/dbFunctions.jsx
@@ -1,10 +1,11 @@
 import { db } from "../firebase/firebaseConfig";
-import { doc, setDoc } from "firebase/firestore";
+import { doc, setDoc, collection } from "firebase/firestore";
 
 export const addUserToCollection = async (user) => {
   try {
-    const userRef = doc(db, "users", user.uid);
-    const userObj = { ...user }; // Create a shallow copy of the user object
+    const usersCollectionRef = collection(db, "users");
+    const userRef = doc(usersCollectionRef, user.uid);
+    const userObj = { email: user.email }; // Create a shallow copy of the user object
     await setDoc(userRef, userObj);
     console.log("User added to collection:", user);
   } catch (error) {
